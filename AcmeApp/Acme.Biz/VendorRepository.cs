b11e0901aa8e2af7d65feeceac677c4922362414
blackmage37 <ben.watsaEapW6roXmllon@nfu.org.uk>
2019-02-26 11:02:11 +0000

    created generic method, updated unit tests

--- AcmeApp/Acme.Biz/VendorRepository.cs
+++ AcmeApp/Acme.Biz/VendorRepository.cs
@@ -29,6 +29,15 @@ namespace Acme.Biz
             return vendor;
         }
 
+        public T RetrieveValue<T>(string sql, T defaultValue)
+        {
+            // call database to retrieve value
+            // if not value returned, return default value
+            T value = defaultValue;
+
+            return value;
+        }
+
         /// <summary>
         /// Save data for one vendor.
         /// </summary>
